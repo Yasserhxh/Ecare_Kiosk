f78112a6a9bc8851445a4bc84efe34d7fd948c88
Yassine Walid <y.walid@alexsys.solutions>
2025-11-13 23:05:49 +0100

    Get Lines

---
+++ src/Ecare.Domain/ValueObjects/CementMatrixRowVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecare.Domain.ValueObjects
+{
+    public sealed record CementMatrixRowVm(
+    int CimentId,
+    string Product,  // "Id - Name"
+    string Type,     // SAC / VRAC / PAL / ...
+    IReadOnlyList<CementLineVm> Lines);
+}
