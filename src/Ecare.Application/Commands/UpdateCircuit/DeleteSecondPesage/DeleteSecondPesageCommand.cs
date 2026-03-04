07795f98194692001ce3450475749f39cf512992
Yassine Walid <y.walid@alexsys.solutions>
2026-03-04 10:57:10 +0000

    .

---
+++ src/Ecare.Application/Commands/UpdateCircuit/DeleteSecondPesage/DeleteSecondPesageCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecare.Application.Commands.UpdateCircuit.DeleteSecondPesage
+{
+    public class DeleteSecondPesageCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+        public required string SecondPesageCanceledBy { get; set; }
+    }
+}
